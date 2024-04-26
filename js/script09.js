a4e19bdaccff274cdee88c7b5e24a15b3167985b
Igor Yurovsky <85400723+Mastermind2025@users.noreply.github.com>
2024-04-26 13:35:32 +0300

    go on

--- js/script09.js
+++ js/script09.js
@@ -38,9 +38,72 @@ const players = [
 // console.log(newPlayers);
 
 //// переписали тоже самое с тернарным
-const newPlayers = players.map((player, index, array) =>
-  player.name === 'Poly'
-    ? { ...player, name: 'Sarah', }
-    : player,
-);
-console.log(newPlayers);
+// const newPlayers = players.map((player, index, array) =>
+//   player.name === 'Poly'
+//     ? { ...player, name: 'Sarah', }
+//     : player,
+// );
+// console.log(newPlayers);
+
+//// вычисляемое свойство
+// const user = {
+//   name: 'Mango',
+// };
+// const key = 'name';
+// console.log(user['name']);
+
+const tweets = [
+  { id: '000', likes: 5, tags: ['js', 'nodejs']},
+  { id: '001', likes: 2, tags: ['html', 'css']},
+  { id: '002', likes: 17, tags: ['html', 'css', 'nodejs']},
+  { id: '003', likes: 8, tags: ['css', 'react']},
+  { id: '004', likes: 0, tags: ['js', 'nodejs', 'react']},
+];
+// console.log(tweets);
+
+////Достаем массивы из объектов. Создаём пустой массив аккумулятор и
+//// складываем в него массивы. Получаем новый массив
+// let allTags = tweets.reduce((acc, tweet) => {
+//    // allTags.push(tweet.tags);
+//   // return allTags;
+//    return [...acc, ...tweet.tags];
+//   // console.log(tweet);
+//   // console.log(acc);
+// }, []);
+// console.log(allTags);
+
+////Тоже самое. Быстрый возврат. Короткая запись
+// let allTags = tweets.reduce((acc, tweet) => [...acc, ...tweet.tags], []);
+// console.log(allTags);
+////Это что в консоль
+////["js", "nodejs", "html", "css", "html", "css", "nodejs", "css", "react",
+//// "js", "nodejs", "react"];
+
+//  //мутебельный вариант
+// let a = 1;
+// const allTags = ["js", "nodejs", "html", "css", "html", "css", "nodejs", "css", "react",
+//   "js", "nodejs", "react"];
+// console.log(allTags);
+// const tagsStats = allTags.reduce((acc, tag) => {
+//   console.log(acc);
+//   if (acc[tag]) {
+//     // acc[tag] += 1;
+// ////иммутабельный вариант. Не изменяет исходный массива
+//     return {
+//       ...acc,
+//       [tag]: acc[tag] + 1
+//     };
+//   }
+//   acc[tag] = 1;
+//   return {
+//     ...acc,
+//     [tag]: 1,
+//   }
+// }, {});
+// console.log(tagsStats);
+
+console.log('a');
+
+
+
+
